cee53490d6ba909564f20d12c65301ca80ac7844
Timofey21 <timofey.milov41@gmail.com>
2021-06-11 21:24:02 +1000

    bla bla

--- main.go
+++ main.go
@@ -81,5 +81,4 @@ func main() {
 	fmt.Println(count)
 
 
-
 }
--- verification.go
+++ verification.go
@@ -1,21 +1,29 @@
 package main
 
 import (
+	"context"
 	"github.com/chromedp/cdproto/page"
 	"github.com/chromedp/chromedp"
+
 )
 
+func verification()  {
+	ctx, cancel := chromedp.NewContext(context.Background())
+	defer cancel()
+
+	chromedp.ListenTarget(ctx, func(ev interface{}) {
+		if _, ok := ev.(*page.EventJavascriptDialogOpening); ok {
+			t := page.HandleJavaScriptDialog(false)
+			go func() {
+				if err := chromedp.Run(ctx, t); err != nil {
+					// handle error
+				}
+
+				// ok
+			}()
+		}
+	})
+
+}
 
 
-chromedp.ListenTarget(ctx, func(ev interface{}) {
-	if _, ok := ev.(*page.EventJavascriptDialogOpening); ok {
-		t := page.HandleJavaScriptDialog(false)
-		go func() {
-			if err := chromedp.Run(ctx, t); err != nil {
-				// handle error
-			}
-
-			// ok
-		}()
-	}
-})
