c98247344c297c0b24e1e0a90ee60026dd0035f8
Timofey21 <timofey.milov41@gmail.com>
2021-06-16 17:14:26 +1000

    new verification

--- main.go
+++ main.go
@@ -4,104 +4,80 @@ import (
 	"bufio"
 	"flag"
 	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
 	"net/url"
 	"os"
-	"strings"
 )
-
-
-func VerifyReflection(body, payload string) bool {
-	if strings.Contains(body, payload) {
-		return true
-	}
-	return false
-}
-
-//func VerifyDOM(s string) bool { //(body io.ReadCloser) bool {
-//
-//	body := ioutil.NopCloser(strings.NewReader(s)) // r type is io.ReadCloser
-//	defer body.Close()
-//
-//	// Load the HTML document
-//	doc, err := goquery.NewDocumentFromReader(body)
-//	check := false
-//	if err != nil {
-//		fmt.Println(err)
-//		return false
-//	}
-//	// Find the review items
-//	doc.Find(".dalfox").Each(func(i int, s *goquery.Selection) {
-//		check = true
-//	})
-//	if !check {
-//		doc.Find("dalfox").Each(func(i int, s *goquery.Selection) {
-//			// For each item found, get the band and title
-//			check = true
-//		})
-//	}
-//	return check
-//}
 
 func main() {
 
 	attackUrl := flag.String("url", "", "url")
 	flag.Parse()
 
-	//var proxy = "http://127.0.0.1:8080"
-	//proxyURL, err := url.Parse(proxy)
-	//
-	//if err != nil {
-	//	log.Println(err)
-	//}
-	//
-	//transport := &http.Transport{
-	//	Proxy: http.ProxyURL(proxyURL),
-	//}
-	//
-	//client := &http.Client{
-	//	Transport: transport,
-	//}
+	var proxy = "http://127.0.0.1:8080"
+	proxyURL, err := url.Parse(proxy)
 
-	f, err := os.Open("xssPayloads.txt")
+	if err != nil {
+		log.Println(err)
+	}
+
+	transport := &http.Transport{
+		Proxy: http.ProxyURL(proxyURL),
+	}
+
+	client := &http.Client{
+		Transport: transport,
+	}
+
+	f, err := os.Open("NewPayloads.txt")
 	if err != nil {
 		panic(err)
 	}
 	defer f.Close()
 
-
 	count := 0
 	sc := bufio.NewScanner(f)
 	for sc.Scan() {
 		xss := sc.Text()
-		xssVector:= *attackUrl + url.QueryEscape(xss)
+		xssVector := *attackUrl + url.QueryEscape(xss)
 
-		verification(xssVector)
+		count++
 
+		//start := time.Now()
+		//go verification(xssVector)
+		//duration := time.Since(start)
+		//fmt.Println(duration)
 
-	//	resp, err := client.Get(xssVector)
-	//	if err != nil {
-	//		log.Println(err)
-	//	}
-	//
-	//	if resp.StatusCode == http.StatusOK {
-	//		bodyBytes, err := ioutil.ReadAll(resp.Body)
-	//		if err != nil {
-	//			log.Fatal(err)
-	//		}
-	//		bodyString := string(bodyBytes)
-	//		if VerifyReflection(bodyString, xss){
-	//			verification(xssVector)
-	//		}
-	//	}
-	//
-	//	err = resp.Body.Close()
-	//	if err != nil {
-	//		log.Println(err)
-	//	}
-	//
+		resp, err := client.Get(xssVector)
+		if err != nil {
+			log.Println(err)
+		}
+
+		if resp.StatusCode == http.StatusOK {
+			bodyBytes, err := ioutil.ReadAll(resp.Body)
+			if err != nil {
+				log.Fatal(err)
+			}
+			bodyString := string(bodyBytes)
+
+			vr := VerifyReflection(bodyString, xss)
+			vd := VerifyDOM(bodyString)
+
+			if vr && vd {
+				verifyChromedp(xssVector)
+				//fmt.Println("XSS Found [+]		" + xssVector)
+			}
+		}
+
+		err = resp.Body.Close()
+		if err != nil {
+			log.Println(err)
+		}
+
 	}
 
 	fmt.Println(count)
-
 
 }
--- verification.go
+++ verification.go
@@ -3,13 +3,15 @@ package main
 import (
 	"context"
 	"fmt"
+	"github.com/PuerkitoBio/goquery"
 	"github.com/chromedp/cdproto/page"
 	"github.com/chromedp/chromedp"
+	"io/ioutil"
 	"log"
+	"strings"
 )
 
-
-func verification(url string)  {
+func verifyChromedp(url string) {
 
 	//opts := append(chromedp.DefaultExecAllocatorOptions[:],
 	//	chromedp.ProxyServer("http://127.0.0.1:8080"),
@@ -22,30 +24,56 @@ func verification(url string)  {
 	ctx, cancel := chromedp.NewContext(context.Background())
 	defer cancel()
 
-
 	chromedp.ListenTarget(ctx, func(ev interface{}) {
 		if _, ok := ev.(*page.EventJavascriptDialogOpening); ok {
-			fmt.Println("Found XSS [+]		" + url)
-			t := page.HandleJavaScriptDialog(false)
+			fmt.Println("Found XSS [+]	" + url)
+			t := page.HandleJavaScriptDialog(true)
 			go func() {
 				if err := chromedp.Run(ctx, t); err != nil {
 					log.Fatal(err)
 				}
 			}()
 		} else {
-				//fmt.Println("No XSS")
+			//fmt.Println("No XSS")
 		}
 	})
 
-
 	err := chromedp.Run(ctx,
 		chromedp.Navigate(url),
-
 	)
 	if err != nil {
 		log.Fatal(err)
 	}
-
 }
 
+func VerifyReflection(body, payload string) bool {
+	if strings.Contains(body, payload) {
+		return true
+	}
+	return false
+}
 
+func VerifyDOM(s string) bool { //(body io.ReadCloser) bool {
+
+	body := ioutil.NopCloser(strings.NewReader(s)) // r type is io.ReadCloser
+	defer body.Close()
+
+	// Load the HTML document
+	doc, err := goquery.NewDocumentFromReader(body)
+	check := false
+	if err != nil {
+		fmt.Println(err)
+		return false
+	}
+	// Find the review items
+	doc.Find(".xss").Each(func(i int, s *goquery.Selection) {
+		check = true
+	})
+	if !check {
+		doc.Find("xss").Each(func(i int, s *goquery.Selection) {
+			// For each item found, get the band and title
+			check = true
+		})
+	}
+	return check
+}
