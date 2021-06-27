cd2f0cdfb73a64fb96a0ab8c621812c5e576429a
Timofey21 <timofey.milov41@gmail.com>
2021-06-27 15:02:08 +1000

    fix

--- pkg/read/args.go
+++ pkg/read/args.go
@@ -1,6 +1,10 @@
 package read
 
-import "flag"
+import (
+	"flag"
+	"fmt"
+	"os"
+)
 
 var Prox *string
 
@@ -8,6 +12,11 @@ func Args() string {
 	attackUrl := flag.String("url", "", "url")
 	Prox = flag.String("proxy", "no proxy", "proxy")
 	flag.Parse()
+
+	if *attackUrl == ""{
+		fmt.Println("Please add target url")
+		os.Exit(0)
+	}
 
 	return *attackUrl
 }
